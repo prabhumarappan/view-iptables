029160ca600bde62f0a2c2e3a2150bb1d3811c58
Prabhu Marappan <prabhum.794@gmail.com>
2020-08-24 05:20:24 +0530

    Added home screen template

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,14 +1,23 @@
 <template>
-  <div id="home-screen">
-    <div class="row">
-        Hello World from Ip Tables
-    </div>
-  </div>
+  <b-container>
+    <navbar></navbar>
+    <b-row>
+      <text-input></text-input>
+    </b-row>
+    <div class="row mb-4"></div>
+    <b-row>
+<!--      <template-home></template-home>-->
+    </b-row>
+  </b-container>
 </template>
 
 <script>
+import TextInput from "./TextInput";
+// import TemplateHome from "./templates/TemplateHome";
+import Navbar from "./Navbar";
 export default {
-  name: "Home"
+  name: "Home",
+  components: {TextInput, Navbar},
 }
 </script>
 
