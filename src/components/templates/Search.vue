b54b63e334ac09cc98810304434d74715472aef5
Prabhu Marappan <prabhum.794@gmail.com>
2020-08-24 05:13:23 +0530

    Added `templates` as a folder

--- src/components/templates/Search.vue
+++ src/components/templates/Search.vue
@@ -4,7 +4,7 @@
 
 <script>
 export default {
-name: "Templates"
+name: "Search"
 }
 </script>
 
---
+++ src/components/templates/TemplateHome.vue
@@ -0,0 +1,32 @@
+<template>
+  <b-container>
+    <navbar></navbar>
+    <b-row align-h="center">
+      <b-col cols="4">
+        <div>
+          Most popular templates:
+          <ol>
+            <li>MongoDB</li>
+            <li>Couchbase DB</li>
+            <li>InfluxDB</li>
+            <li>API Server</li>
+            <li>FluentD</li>
+            <li>ElasticSearch & Kibana</li>
+          </ol>
+        </div>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+import Navbar from "../Navbar";
+export default {
+  name: "TemplateHome",
+  components: {Navbar}
+}
+</script>
+
+<style scoped>
+
+</style>
