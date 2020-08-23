57e2c51a200be90c76bb96e42f911c041b1e9ce9
Prabhu Marappan <prabhum.794@gmail.com>
2020-08-24 05:19:52 +0530

    Adapter to router

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <Home />
+    <router-view/>
   </div>
 </template>
 
 <script>
 
-import Home from "./components/Home";
 
 export default {
   name: 'App',
   components: {
-    Home,
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
+import '@babel/polyfill'
+import 'mutationobserver-shim'
 import Vue from 'vue'
-import App from './App.vue'
+import './plugins/bootstrap-vue'
+import router from './router'
+import App from "./App";
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
