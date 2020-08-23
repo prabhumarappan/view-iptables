c2b7aaec47ae4e4896be2eb9ef222f42ddf50d98
Prabhu Marappan <prabhum.794@gmail.com>
2020-08-24 05:21:00 +0530

    Added click function to find out debug message

--- src/components/TextInput.vue
+++ src/components/TextInput.vue
@@ -1,10 +1,29 @@
 <template>
-
+  <b-container>
+    <b-row align-h="center">
+      <b-col cols="6">
+        <b-form-input v-model="rule" placeholder="Enter the rule" wid></b-form-input>
+      </b-col>
+      <b-col cols="1">
+        <b-button variant="success" v-on:click="convertRule">Submit</b-button>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
 export default {
-  name: "TextInput"
+  name: "TextInput",
+  data: function () {
+    return {
+      rule: null,
+    }
+  },
+  methods: {
+    convertRule: function () {
+      console.log(this.rule);
+    }
+  }
 }
 </script>
 
